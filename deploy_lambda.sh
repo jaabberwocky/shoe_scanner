75d33e7cc77ffd6fd52eafce2c40438ea8557ce1
tobias <tobiasleongzhunmun@gmail.com>
2018-12-03 03:14:34 +0800

    rm deploy_lambda.sh and used deployment.sh for building and sending to Lambda

--- deploy_lambda.sh
+++
@@ -1,9 +0,0 @@
-cd venv/Lib/site-packages
-zip -r9 ../../../function.zip .
-
-cd ../../..
-zip -g function.zip application.py
-zip -g function.zip ARN.py
-zip -g function.zip config.py
-
-aws lambda update-function-code --function-name shoe_scanner --zip-file fileb://function.zip
---
+++ deployment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Removing dist folder..."
+rm -rf dist
+echo "Creating dist folder"
+mkdir -p dist
+echo "Copying site-packages libraries..."
+cp -rf venv/Lib/site-packages/* dist
+echo "Copying application code..."
+cp application.py dist
+cp config.py dist
+cp ARN.py dist
+
+cd dist
+
+echo "Zipping file..."
+zip -r9 ../function.zip .
+
+cd ..
+
+aws lambda update-function-code --function-name shoe_scanner --zip-file fileb://function.zip
